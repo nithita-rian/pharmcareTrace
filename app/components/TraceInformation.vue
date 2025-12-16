28c50cecc0cdf52783139d4ecc76614fee4d029c
Oue_Nithita <oue.nithita@gmail.com>
2025-12-17 05:17:59 +0700

    Feat: Add Trace Information components

---
+++ app/components/TraceInformation.vue
@@ -0,0 +1,77 @@
+<!-- TraceInformation.vue -->
+<template>
+    <div class="bg-white rounded-lg shadow-lg p-6">
+        <div class="grid grid-cols-1 md:grid-cols-12 gap-8">
+
+            <div class="md:col-span-5 flex flex-col items-center justify-center">
+                <div class="w-full p-2 bg-gray-50 flex items-center justify-center border rounded-lg mb-4">
+                    <img src="/tripala.jpg" alt="tripala">
+                </div>
+            </div>
+
+            <div class="md:col-span-7">
+                <h2 class="text-xl font-bold underline mb-4 text-gray-900">สรุปการตรวจสอบย้อนกลับ</h2>
+
+                <div v-if="product" class="space-y-2 text-base">
+                    <div class="grid grid-cols-3">
+                        <span class="font-bold">สินค้า :</span>
+                        <span class="col-span-2 text-gray-600">{{ product.name }}</span>
+                    </div>
+                    <div class="grid grid-cols-3">
+                        <span class="font-bold">จาก :</span>
+                        <span class="col-span-2 text-gray-600">{{ product.produced }}</span>
+                    </div>
+                    <div class="grid grid-cols-3">
+                        <span class="font-bold">รหัสสินค้า :</span>
+                        <span class="col-span-2 text-gray-600">{{ product.productCode }}</span>
+                    </div>
+                    <div class="grid grid-cols-3">
+                        <span class="font-bold">รหัสบรรจุภัณฑ์ :</span>
+                        <span class="col-span-2 text-gray-600">{{ product.packageId }}</span>
+                    </div>
+                    <div class="grid grid-cols-3">
+                        <span class="font-bold">รหัสการขนส่ง :</span>
+                        <span class="col-span-2 text-gray-600">{{ product.transportId }}</span>
+                    </div>
+                    <div class="grid grid-cols-3">
+                        <span class="font-bold">วันที่ผลิต :</span>
+                        <span class="col-span-2 text-gray-600">{{ product.produceDate }}</span>
+                    </div>
+                    <div class="grid grid-cols-3">
+                        <span class="font-bold">แหล่งที่มาของการแปรรูป :</span>
+                        <span class="col-span-2 text-gray-600">{{ product.processingAddress }}</span>
+                    </div>
+                    <div class="grid grid-cols-3">
+                        <span class="font-bold">วันที่บรรจุ :</span>
+                        <span class="col-span-2 text-gray-600">{{ product.packDate }}</span>
+                    </div>
+                    <div class="grid grid-cols-3">
+                        <span class="font-bold">วันที่จัดส่ง :</span>
+                        <span class="col-span-2 text-gray-600">{{ product.shipDate }}</span>
+                    </div>
+                    <div class="grid grid-cols-3">
+                        <span class="font-bold">ประเภทการขนส่ง :</span>
+                        <span class="col-span-2 text-gray-600">{{ product.transportType }}</span>
+                    </div>
+                    <div class="grid grid-cols-3">
+                        <span class="font-bold">หมายเลขตู้คอนเทนเนอร์ :</span>
+                        <span class="col-span-2 text-green-800 font-bold">{{ product.containerNo }}</span>
+                    </div>
+                    <div class="grid grid-cols-3">
+                        <span class="font-bold">หมายเลขติดตาม :</span>
+                        <span class="col-span-2 text-gray-600">{{ product.trackingNo }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    product: Object,
+});
+</script>
+
+<style></style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
-    <h1 class="text-base text-red-500">index</h1>
-  </div>
+    <div>
+        <TraceInformation :product="product"/>
+    </div>
 </template>
+<script setup>
+// Mock Data based on the image
+const product = reactive({
+    name: 'น้ำตรีผลา สูตร LOWSUGAR',
+    productCode: '44-2-01055-2-0001',
+    produced: 'โรงงานฟาร์มแคร์ ฟาร์มาซูติคอล คณะเภสัชศาสตร์ มหาวิทยาลัยมหาสารคาม',
+    processingAddress: 'โรงงานฟาร์มแคร์ ฟาร์มาซูติคอล คณะเภสัชศาสตร์ มหาวิทยาลัยมหาสารคาม',
+    packageId: '16011557416009',
+    transportId: '16660115576873730',
+    produceDate: '2025-12-07',
+    packDate: '2025-12-08',
+    shipDate: '2025-12-09',
+    transportType: 'Truck',
+    containerNo: '123456789',
+    trackingNo: 'UFQHFY'
+});
+</script>
+
+<style></style>
