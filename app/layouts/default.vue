001a93608c11bb6f8f967c34533984dc7c9faefc
Oue_Nithita <oue.nithita@gmail.com>
2025-12-17 04:11:58 +0700

    setup project

---
+++ app/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="min-h-screen">
+    <header>header</header>
+    <main class="mx-auto px-4 py-8 min-h-screen bg-white">
+      <slot />
+    </main>
+    <footer class="bg-gray-100 inset-shadow-sm sticky bottom-0">
+      <div class="px-4 py-4 flex items-center space-x-2 md:px-8 md:py-6">
+        <Icon name="mdi:copyright" class="w-4 h-4 md:w-6 md:h-6 text-gray-400" />
+        <span class="text-sm text-gray-600 md:text-base"> 2025 All Rights Reserved</span>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="text-2xl">index</h1>
+  </div>
+</template>
